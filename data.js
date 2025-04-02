a0725918d9e6907c198e83d09346fb31423215d6
pirate066 <derevshchukov@gmail.com>
2025-04-02 03:09:07 +0300

    111

---
+++ data.js
@@ -0,0 +1,41 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-ballroom",
+      "name": "ballroom",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        }
+      ],
+      "faceSize": 320,
+      "initialViewParameters": {
+        "yaw": 0,
+        "pitch": 0,
+        "fov": 1.5785051123454943
+      },
+      "linkHotspots": [],
+      "infoHotspots": [
+        {
+          "yaw": 0,
+          "pitch": 0,
+          "title": "111",
+          "text": "222"
+        }
+      ]
+    }
+  ],
+  "name": "360",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
